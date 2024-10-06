22d2977adf3242ef6881b886364bb6e5b5b04c62
Evelyn Si <evelyna.217@gmail.com>
2024-10-06 15:43:28 -0400

    Computed infection of rate of connections

--- model.py
+++ model.py
@@ -16,8 +16,26 @@ status_terms = ['never infected',
 N=107000
 population=np.zeros(N, dtype=person)
 
-population['connectivity'] = N*np.random.random( size=N)
+population['connectivity'] = N*np.random.random(size=N)
 population['severity'] = 10*np.random.random(size=N)
 population['days_infectious'] = np.random.randint(0, 8, size=N)  
 population['is_sick'] = population['days_infectious'] > 0
 population['status'] = np.random.choice(status_terms, size=N)
+
+
+
+# compute infection of rate of connections
+sick_population=population[population['is_sick']==True]
+sum_connectivity_sick = np.sum(sick_population['connectivity'])
+total_connectivity=np.sum(population['connectivity'])
+infection_rate=sum_connectivity_sick/total_connectivity
+
+
+not_sick_population=population[population['is_sick']==False]
+
+
+    
+
+
+
+
